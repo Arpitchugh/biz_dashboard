2eec9e2a1fab8d8848a7b08b919112bdbb61a5ca
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2023-11-21 13:41:08 +0530

    Add Box component to main layout

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import SideNav from './components/sideNav';
-
+import { Box } from '@mui/material';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -14,7 +14,17 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={inter.className}>
 				<SideNav />
-				{children}
+				<Box
+					component='main'
+					sx={{
+						flexGrow: 1,
+						ml: `240px`,
+						mt: ['48px', '56px', '64px'],
+						p: 3,
+					}}
+				>
+					{children}
+				</Box>
 			</body>
 		</html>
 	);
