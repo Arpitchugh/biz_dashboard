b103d760dbcb3bc5a878aba0c138b3d6c69a1a46
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2023-11-22 15:11:11 +0530

    card comp of overview

--- app/(overview)/page.js
+++ app/(overview)/page.js
@@ -3,32 +3,101 @@ import React from 'react';
 import { Card, CardContent, Typography, Box } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import theme from '../ui/theme';
-
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import CreditScoreIcon from '@mui/icons-material/CreditScore';
+import TrackChangesIcon from '@mui/icons-material/TrackChanges';
 const cardData = [
-	{ amount: '₹2.56 Cr', label: 'Currently Raising' },
-	{ amount: '₹5.98 Cr', label: 'Total Raised' },
-	{ amount: '₹0.65 Cr', label: 'Available Target Raise' },
+	{
+		icon: TrendingUpIcon,
+		heading: 'Currently Raising',
+		amount: '₹2.56 Cr',
+		label: 'To be settled by',
+		date: '7 Nov 2023 ',
+	},
+	{
+		icon: TrackChangesIcon,
+		heading: 'Target Raise',
+		amount: '₹2.56 Cr',
+		label: 'more can be raised',
+		etc: 'know more',
+	},
+	{
+		icon: CreditScoreIcon,
+		heading: 'Current Exposure',
+		amount: '₹70 L',
+		label: 'To be settled by',
+		date: '25 Nov 2023',
+	},
 ];
+
+const CardComponent = ({ icon: Icon, heading, amount, label, date, etc }) => {
+	return (
+		<Card className='min-w-[160px] m-2 rounded-xl '>
+			<CardContent className='flex flex-col pt-6 pl-6 pr-20'>
+				<Box className='flex gap-4 items-center mb-5'>
+					<Icon />
+					<Typography variant='h5'>
+						{heading}
+					</Typography>
+				</Box>
+				<Box sx={{ paddingLeft: '40px' }}>
+					<Typography variant='h1'>
+						{amount}
+					</Typography>
+					<Box className='flex flex-col mt-2.5'>
+						<Typography
+							variant='subtitle1'
+							className='text-neutral-400'
+						>
+							{label}
+						</Typography>
+						<Typography
+							variant='h3'
+							sx={
+								etc && {
+									textDecoration:
+										'underline',
+									color: '#6D8FFF',
+									fontSize: '14px',
+								}
+							}
+						>
+							{date ? date : etc}
+						</Typography>
+					</Box>
+				</Box>
+			</CardContent>
+		</Card>
+	);
+};
 
 const overview = () => {
 	return (
 		<ThemeProvider theme={theme}>
-			<div className='flex flex-col gap-6 '>
+			<Box className='flex flex-col gap-9 '>
 				{/* Heading Section */}
 				<Box>
-					<Typography variant='h1'>
+					<Typography variant='h2'>
 						Overview
 					</Typography>
 					<Typography
-						variant='h3'
-						className='stone500 '
+						variant='h4'
+						className='text-neutral-500'
 					>
 						Gives you a overview of all the
 						Deals happening Today
 					</Typography>
 				</Box>
 				{/* Cards Section */}
-			</div>
+				<Box className='flex items-center flex-wrap '>
+					{cardData.map((metric, index) => (
+						<CardComponent
+							key={index}
+							{...metric}
+						/>
+					))}
+				</Box>
+			</Box>
 		</ThemeProvider>
 	);
 };
